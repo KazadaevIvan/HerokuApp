a3507251dc0b8aa9c9ee0bf1e4a6ce8a734bbfe7
Ivan Kazadaev <i.ziliboba@mail.ru>
2020-09-20 19:18:45 +0300

    Added BaseTest

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,17 +1,10 @@
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
 
-import java.util.List;
 import java.util.concurrent.TimeUnit;
-
-import static org.testng.Assert.assertFalse;
-import static org.testng.Assert.assertTrue;
 
 public class BaseTest {
 
