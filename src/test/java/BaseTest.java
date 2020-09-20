77ce97a6bf4219583099aacd3943b3790a8c9c9e
Ivan Kazadaev <i.ziliboba@mail.ru>
2020-09-20 19:17:30 +0300

    Added BaseTest

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+public class BaseTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.setHeadless(false);
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.get("http://the-internet.herokuapp.com/");
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
