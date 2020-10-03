dea9d3f753b55fef6b0faaf5658c6f02e65edf44
Ivan Kazadaev <i.ziliboba@mail.ru>
2020-10-03 14:37:49 +0300

    Updated test method's name

---
+++ src/test/java/ContextMenuTest.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class ContextMenuTest extends BaseTest {
+
+    @Test
+    public void contextMenuTest() {
+        driver.findElement(By.linkText("Context Menu")).click();
+        Actions actions = new Actions(driver);
+        actions.contextClick(driver.findElement(By.id("hot-spot"))).perform();
+        Alert alert = driver.switchTo().alert();
+        String expectedResult = "You selected a context menu";
+        System.out.println(alert.getText());
+        assertEquals(alert.getText(), expectedResult, "Alert message should be " + expectedResult);
+        alert.accept();
+    }
+}
