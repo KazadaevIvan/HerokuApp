02cfbceac96744f46c17525e99de6ee5bd4a5407
Ivan Kazadaev <i.ziliboba@mail.ru>
2020-10-03 16:36:44 +0300

    Added spaces

--- src/test/java/DynamicControlsTest.java
+++ src/test/java/DynamicControlsTest.java
@@ -1,5 +1,4 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
@@ -16,14 +15,14 @@ public class DynamicControlsTest extends BaseTest {
         driver.findElement(By.linkText("Dynamic Controls")).click();
         driver.findElement(By.id("checkbox"));
         driver.findElement(By.cssSelector("[onclick='swapCheckbox()']")).click();
-        new WebDriverWait(driver,20)
+        new WebDriverWait(driver, 20)
                 .until(ExpectedConditions.visibilityOfElementLocated(By.id("message")));
         driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
         assertTrue(driver.findElements(By.id("checkbox")).isEmpty(), "Checkbox should not be displayed");
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         assertFalse(driver.findElement(By.tagName("input")).isEnabled(), "Input should be disabled");
         driver.findElement(By.cssSelector("[onclick='swapInput()']")).click();
-        new WebDriverWait(driver,20)
+        new WebDriverWait(driver, 20)
                 .until(ExpectedConditions.visibilityOfElementLocated(By.id("message")));
         assertTrue(driver.findElement(By.tagName("input")).isEnabled(), "Input should be enabled");
     }
