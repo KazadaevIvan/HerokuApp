3fc1c7ccbcb483883e914ffe1547d2eac603c650
Ivan Kazadaev <i.ziliboba@mail.ru>
2020-10-06 19:59:20 +0300

    Updated path to the file in fileUploadTest method

--- src/test/java/FileUploadTest.java
+++ src/test/java/FileUploadTest.java
@@ -3,6 +3,8 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
+import java.io.File;
+
 import static org.testng.Assert.assertEquals;
 
 public class FileUploadTest extends BaseTest {
@@ -10,7 +12,8 @@ public class FileUploadTest extends BaseTest {
     @Test
     public void fileUploadTest() {
         driver.findElement(By.linkText("File Upload")).click();
-        driver.findElement(By.id("file-upload")).sendKeys("C:/projects/HerokuApp/src/test/resources/bug.jpg");
+        File file = new File("src/test/resources/bug.jpg");
+        driver.findElement(By.id("file-upload")).sendKeys(file.getAbsolutePath());
         driver.findElement(By.id("file-submit")).click();
         new WebDriverWait(driver, 20)
                 .until(ExpectedConditions.visibilityOfElementLocated(By.id("uploaded-files")));
